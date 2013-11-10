13c36beabf03fe098bc2fb3526282b2cbd40be0d
Alan Jenkins <alan.james.jenkins@gmail.com>
2013-11-10 11:19:16 +0000

    More pacgen refactoring.

--- pacgen.py
+++ pacgen.py
@@ -6,12 +6,19 @@ Minecraft mod packs.
 import xml.etree.ElementTree as ET
 import urllib2
 import json
+import pdb
 
-MINECRAFT_VERSION = '1.6.2'
+MINECRAFT_VERSION = '1.6.4'
 BOT_URL = "http://bot.notenoughmods.com/%s.json" % MINECRAFT_VERSION
 
-BOT_MODS_RESPONSE = urllib2.urlopen(BOT_URL)
-BOT_MOD_LIST = json.loads(BOT_MODS_RESPONSE.read())
+
+def get_bot_mods():
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36"
+    request = urllib2.Request(BOT_URL, headers=headers)
+    BOT_MODS_RESPONSE = urllib2.urlopen(request)
+    BOT_MOD_LIST = json.loads(BOT_MODS_RESPONSE.read())
+    return BOT_MOD_LIST
 
 
 def find_mod_version(bot_mod_list, mod_name):
@@ -26,6 +33,7 @@ def find_mod_version(bot_mod_list, mod_name):
     return None
 
 
+BOT_MOD_LIST = get_bot_mods()
 PACK_XML_TREE = ET.parse('../pack.xml')
 PACK_XML_ROOT = PACK_XML_TREE.getroot()
 with open('../pacgen_report.html', 'w') as linksfile:
@@ -40,40 +48,43 @@ with open('../pacgen_report.html', 'w') as linksfile:
     </tr>"""
     for mod in PACK_XML_ROOT[2]:
         current_version = find_mod_version(BOT_MOD_LIST, mod.attrib['name'])
+        mod_version_no_mc = \
+            mod.attrib['version'].replace('%s-'
+                                          % MINECRAFT_VERSION, '')
 
         # pdb.set_trace()
-        try:
-            if not current_version:
-                current_version = {}
-                current_version['version'] = "Unknown"
-                current_version['longurl'] = "Unknown"
-                mod_version_no_mc = \
-                    mod.attrib['version'].replace('%s-'
-                                                  % MINECRAFT_VERSION, '')
-            if current_version['version'] == 'dev-only':
-                if current_version['dev'] != mod_version_no_mc:
-                    PACK_REPORT_HTML += "<tr><td><a href=\"%s\">%s</a></td>" \
-                        % (mod.attrib['website'], mod.attrib['name'])
-                    if current_version['longurl'] != "Unknown":
-                        PACK_REPORT_HTML += "<td><a href=%s>Update</a></td>"\
-                            % current_version['longurl']
-                    else:
-                        PACK_REPORT_HTML += "<td></td>"
-                    PACK_REPORT_HTML += "<td>%s</td><td>%s</td></tr>"\
-                        % (mod.attrib['version'], current_version['dev'])
-            else:
-                if current_version['version'] != mod_version_no_mc:
-                    PACK_REPORT_HTML += "<tr><td><a href=\"%s\">%s</a></td>"\
-                        % (mod.attrib['website'], mod.attrib['name'])
-                    if current_version['longurl'] != "Unknown":
-                        PACK_REPORT_HTML += "<td><a href=%s>Update</a></td>"\
-                            % current_version['longurl']
-                    else:
-                        PACK_REPORT_HTML += "<td></td>"
-                    PACK_REPORT_HTML += "<td>%s</td><td>%s</td></tr>"\
-                        % (mod.attrib['version'], current_version['version'])
-        except:
-            print(mod.attrib['name'])
+        # try:
+        if not current_version:
+            current_version = {}
+            current_version['version'] = "Unknown"
+            current_version['longurl'] = "Unknown"
+            mod_version_no_mc = \
+                mod.attrib['version'].replace('%s-'
+                                              % MINECRAFT_VERSION, '')
+        if current_version['version'] == 'dev-only':
+            if current_version['dev'] != mod_version_no_mc:
+                PACK_REPORT_HTML += "<tr><td><a href=\"%s\">%s</a></td>" \
+                    % (mod.attrib['website'], mod.attrib['name'])
+                if current_version['longurl'] != "Unknown":
+                    PACK_REPORT_HTML += "<td><a href=%s>Update</a></td>"\
+                        % current_version['longurl']
+                else:
+                    PACK_REPORT_HTML += "<td></td>"
+                PACK_REPORT_HTML += "<td>%s</td><td>%s</td></tr>"\
+                    % (mod.attrib['version'], current_version['dev'])
+        else:
+            if current_version['version'] != mod_version_no_mc:
+                PACK_REPORT_HTML += "<tr><td><a href=\"%s\">%s</a></td>"\
+                    % (mod.attrib['website'], mod.attrib['name'])
+                if current_version['longurl'] != "Unknown":
+                    PACK_REPORT_HTML += "<td><a href=%s>Update</a></td>"\
+                        % current_version['longurl']
+                else:
+                    PACK_REPORT_HTML += "<td></td>"
+                PACK_REPORT_HTML += "<td>%s</td><td>%s</td></tr>"\
+                    % (mod.attrib['version'], current_version['version'])
+        # except:
+        #     print(mod.attrib['name'])
 
     PACK_REPORT_HTML += "</table></body></html>"
     linksfile.write(PACK_REPORT_HTML)
